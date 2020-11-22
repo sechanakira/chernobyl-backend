6262488520d4270712c4b8b4c6128f4c6adf58f2
ShingiraiC <shingiraic@vatit.com>
2020-11-22 13:47:39 +0200

    chore: initial commit

---
+++ src/main/java/com/github/sechanakira/chernobylbackend/ChernobylBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.sechanakira.chernobylbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChernobylBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChernobylBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/sechanakira/chernobylbackend/ChernobylBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sechanakira.chernobylbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChernobylBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
